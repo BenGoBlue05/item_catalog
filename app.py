99c0cd184d89c7644c0d2d3fae98777071a735ae
Ben Lewis <bplewis5@gmail.com>
2019-02-25 21:45:51 -0600

    reformat to pep8 compliant

--- app.py
+++ app.py
@@ -16,9 +16,12 @@ import requests
 
 app = Flask(__name__)
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read()
+)['web']['client_id']
 
-engine = create_engine('sqlite:///garagesale.db', connect_args={'check_same_thread': False})
+DB_NAME = 'sqlite:///garagesale.db'
+engine = create_engine(DB_NAME, connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -43,7 +46,11 @@ def add_garage_sale():
     if 'username' not in login_session:
         return redirect(url_for('show_login'))
     if request.method == 'POST':
-        sale = GarageSale(name=request.form['name'], address=request.form['address'], user_id=login_session['user_id'])
+        sale = GarageSale(
+            name=request.form['name'],
+            address=request.form['address'],
+            user_id=login_session['user_id']
+        )
         session.add(sale)
         session.commit()
         flash("The Garage Sale has been successfully created!")
@@ -75,7 +82,10 @@ def edit_garage_sale(garage_sale_id):
         return render_template('edit_garage_sale.html', garage_sale=sale)
 
 
-@app.route('/garagesales/<int:garage_sale_id>/delete/', methods=['GET', 'POST'])
+@app.route(
+    '/garagesales/<int:garage_sale_id>/delete/',
+    methods=['GET', 'POST']
+)
 def delete_garage_sale(garage_sale_id):
     sale = session.query(GarageSale).filter_by(id=garage_sale_id).one()
     if 'username' not in login_session:
@@ -109,10 +119,14 @@ def item_details_json(item_id):
 def show_garage_sale_details(garage_sale_id):
     sale = session.query(GarageSale).filter_by(id=garage_sale_id).one()
     items = session.query(Item).filter_by(garage_sale_id=garage_sale_id).all()
-    return render_template('garage_sale_details.html', garage_sale=sale, products=items)
+    return render_template('garage_sale_details.html',
+                           garage_sale=sale, products=items)
 
 
-@app.route('/garagesales/<int:garage_sale_id>/items/new', methods=['GET', 'POST'])
+@app.route(
+    '/garagesales/<int:garage_sale_id>/items/new',
+    methods=['GET', 'POST']
+)
 def new_item(garage_sale_id):
     if 'username' not in login_session:
         return redirect(url_for('show_login'))
@@ -128,12 +142,16 @@ def new_item(garage_sale_id):
         session.add(item)
         session.commit()
         flash("%s has beed added to your garage sale!" % name)
-        return redirect(url_for('show_garage_sale_details', garage_sale_id=garage_sale_id))
+        return redirect(url_for('show_garage_sale_details',
+                                garage_sale_id=garage_sale_id))
     else:
         return render_template('new_item.html', garage_sale_id=garage_sale_id)
 
 
-@app.route('/garagesales/<int:garage_sale_id>/items/<int:item_id>/edit/', methods=['GET', 'POST'])
+@app.route(
+    '/garagesales/<int:garage_sale_id>/items/<int:item_id>/edit/',
+    methods=['GET', 'POST']
+)
 def edit_item(garage_sale_id, item_id):
     if 'username' not in login_session:
         flash("Please log in to continue.")
@@ -142,7 +160,8 @@ def edit_item(garage_sale_id, item_id):
     item = session.query(Item).filter_by(id=item_id).one()
     if login_session['user_id'] != item.user_id:
         flash("You are not authorized to access that page.")
-        return redirect(url_for('show_garage_sale_details', garage_sale_id=garage_sale_id))
+        return redirect(url_for('show_garage_sale_details',
+                                garage_sale_id=garage_sale_id))
 
     if request.method == 'POST':
         name = request.form['name']
@@ -157,12 +176,17 @@ def edit_item(garage_sale_id, item_id):
         session.add(item)
         session.commit()
         flash("The changes have been saved!")
-        return redirect(url_for('show_garage_sale_details', garage_sale_id=garage_sale_id))
+        return redirect(url_for('show_garage_sale_details',
+                                garage_sale_id=garage_sale_id))
     else:
-        return render_template('edit_item.html', garage_sale_id=garage_sale_id, item=item)
+        return render_template('edit_item.html',
+                               garage_sale_id=garage_sale_id, item=item)
 
 
-@app.route('/garagesales/<int:garage_sale_id>/items/<int:item_id>/delete/', methods=['GET', 'POST'])
+@app.route(
+    '/garagesales/<int:garage_sale_id>/items/<int:item_id>/delete/',
+    methods=['GET', 'POST']
+)
 def delete_item(garage_sale_id, item_id):
     if 'username' not in login_session:
         flash("Please log in to continue.")
@@ -170,14 +194,17 @@ def delete_item(garage_sale_id, item_id):
     item = session.query(Item).filter_by(id=item_id).one()
     if login_session['user_id'] != item.user_id:
         flash("You are not authorized to access that page.")
-        return redirect(url_for('show_garage_sale_details', garage_sale_id=garage_sale_id))
+        return redirect(url_for('show_garage_sale_details',
+                                garage_sale_id=garage_sale_id))
     if request.method == 'POST':
         session.delete(item)
         session.commit()
         flash("Successfully deleted")
-        return redirect(url_for('show_garage_sale_details', garage_sale_id=garage_sale_id))
+        return redirect(url_for('show_garage_sale_details',
+                                garage_sale_id=garage_sale_id))
     else:
-        return render_template('delete_item.html', garage_sale_id=garage_sale_id, item=item)
+        return render_template('delete_item.html',
+                               garage_sale_id=garage_sale_id, item=item)
 
 
 @app.route('/garagesales/<garage_sale_id>/items/<item_id>')
@@ -190,7 +217,8 @@ def show_item_details(garage_sale_id, item_id):
 
 @app.route('/login/')
 def show_login():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for _ in range(32))
     login_session['state'] = state
     return render_template('login.html', STATE=state)
 
@@ -209,13 +237,15 @@ def gconnect():
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
-        response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+        message = 'Failed to upgrade the authorization code.'
+        response = make_response(json.dumps(message), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Check access token validity
     access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    base_url = 'https://www.googleapis.com/oauth2/v1/tokeninfo'
+    url = '{}?access_token={}'.format(base_url, access_token)
     h = httplib2.Http()
     content = h.request(url)[1]
     result = json.loads(content.decode('utf-8'))
@@ -228,13 +258,15 @@ def gconnect():
     # Verify access token for intended user
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
-        response = make_response(json.dumps("Token's user ID doesn't match given user ID."), 401)
+        message = "Token's user ID doesn't match given user ID."
+        response = make_response(json.dumps(message), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Verify access token valid for app
     if result['issued_to'] != CLIENT_ID:
-        response = make_response(json.dumps("Token's client ID does not match app's."), 401)
+        message = "Token's client ID does not match app's."
+        response = make_response(json.dumps(message), 401)
         print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -317,7 +349,11 @@ def logout():
 
 
 def create_user():
-    user = User(name=login_session['username'], email=login_session['email'], img_url=login_session['picture'])
+    user = User(
+        name=login_session['username'],
+        email=login_session['email'],
+        img_url=login_session['picture']
+    )
     session.add(user)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
